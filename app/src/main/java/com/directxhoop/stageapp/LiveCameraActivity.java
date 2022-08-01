35ee5fd52aad38cc70bbf5a97f4fb8f34a4b0df4
KurtKey <baouchouch.saad@gmail.com>
2022-08-01 10:49:50 +0200

    FaceDetectionApp

---
+++ app/src/main/java/com/directxhoop/stageapp/LiveCameraActivity.java
@@ -0,0 +1,127 @@
+package com.directxhoop.stageapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.common.annotation.KeepName;
+
+import java.io.ByteArrayOutputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class LiveCameraActivity extends AppCompatActivity {
+
+
+    private CameraSource cameraSource = null;
+    private CameraSourcePreview preview;
+    private Boolean isFrontFacing = true;
+    private GraphicOverlay graphicOverlay;
+    private static final String TAG = "LiveCameraActivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Log.d(TAG, "onCreate");
+
+        setContentView(R.layout.activity_live_camera);
+
+        preview = findViewById(R.id.preview_view);
+        if (preview == null) {
+            Log.d(TAG, "Preview is null");
+        }
+        graphicOverlay = findViewById(R.id.graphic_overlay);
+        if (graphicOverlay == null) {
+            Log.d(TAG, "graphicOverlay is null");
+        }
+
+        ImageView facingSwitch = findViewById(R.id.facing_switch);
+
+        facingSwitch.setOnClickListener(v -> {
+            isFrontFacing = !isFrontFacing;
+            toggleCamera();
+        });
+
+        createCameraSource();
+        toggleCamera();
+    }
+    // to switch between cameras
+    private void toggleCamera(){
+        Log.d(TAG, "Set facing");
+        if (cameraSource != null) {
+            if (isFrontFacing) {
+                cameraSource.setFacing(CameraSource.CAMERA_FACING_FRONT);
+            } else {
+                cameraSource.setFacing(CameraSource.CAMERA_FACING_BACK);
+            }
+        }
+        preview.stop();
+        startCameraSource();
+    }
+
+    private void createCameraSource() {
+        // If there's no existing cameraSource, create one.
+        if (cameraSource == null) {
+            cameraSource = new CameraSource(this, graphicOverlay);
+        }
+        cameraSource.setMachineLearningFrameProcessor(new FaceDetectorProcessor(this));
+    }
+
+    /**
+     * Starts or restarts the camera source, if it exists. If the camera source doesn't exist yet
+     * (e.g., because onResume was called before the camera source was created), this will be called
+     * again when the camera source is created.
+     */
+    private void startCameraSource() {
+        if (cameraSource != null) {
+            try {
+                if (preview == null) {
+                    Log.d(TAG, "resume: Preview is null");
+                }
+                if (graphicOverlay == null) {
+                    Log.d(TAG, "resume: graphOverlay is null");
+                }
+                preview.start(cameraSource, graphicOverlay);
+            } catch (IOException e) {
+                Log.e(TAG, "Unable to start camera source.", e);
+                cameraSource.release();
+                cameraSource = null;
+            }
+        }
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        Log.d(TAG, "onResume");
+        createCameraSource();
+        startCameraSource();
+    }
+
+    /**
+     * Stops the camera.
+     */
+    @Override
+    protected void onPause() {
+        super.onPause();
+        preview.stop();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        if (cameraSource != null) {
+            cameraSource.release();
+        }
+    }
+}
